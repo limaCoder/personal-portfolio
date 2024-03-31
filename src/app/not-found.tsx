e360ba28050661fa2f51c2a66a12c47353e8d123
Mario Lima <marioaulima15@outlook.com>
2024-03-30 23:03:16 -0300

    refactor: improve 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import { AuroraBackground } from "./components/ui/aurora-background";
 import { MotionDiv } from "./lib/framer-motion/MotionComponents";
 
@@ -14,12 +16,18 @@ export default function NotFound() {
         }}
         className="relative flex flex-col gap-4 items-center justify-center px-4"
       >
-        <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
+        <div className="text-3xl md:text-7xl font-bold text-custom_primary text-center">
           Page or resource not found
         </div>
-        <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
+        <div className="font-extralight text-base md:text-4xl text-neutral-200 py-4">
           404
         </div>
+        <Link
+          className="bg-white rounded-lg w-fit text-black px-4 py-2"
+          href="/"
+        >
+          Back to homepage
+        </Link>
       </MotionDiv>
     </AuroraBackground>
   );
