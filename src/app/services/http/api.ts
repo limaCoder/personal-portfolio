e1852ddd2cf626d5fa2f6d7504e01dc601614526
Mario Lima <marioaulima15@outlook.com>
2024-03-31 22:36:26 -0300

    fix: adjustments in env errors in deploy

--- src/app/services/http/api.ts
+++ src/app/services/http/api.ts
@@ -2,6 +2,11 @@ import { env } from "@/app/lib/env/env";
 
 export function api(path: string, init?: RequestInit) {
   const baseUrl = env.NEXT_PUBLIC_API_BASE_URL;
+
+  if (baseUrl) {
+    return null;
+  }
+
   const url = new URL(path, baseUrl);
 
   return fetch(url, init);
