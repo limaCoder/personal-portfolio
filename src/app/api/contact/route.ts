82a365f2008df69afcfe6174a1f4418b2de522d5
Mario Lima <marioaulima15@outlook.com>
2024-04-07 21:00:14 -0300

    feat: add hygraph envs and fetch query function

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,4 +1,4 @@
-import { env } from "@/app/lib/env/env";
+import { env } from "@/app/lib/env";
 
 import { NextResponse } from "next/server";
 import { z } from "zod";
--- src/app/services/notion/projects/index.ts
+++ src/app/services/notion/projects/index.ts
@@ -3,7 +3,7 @@
 import { Client } from "@notionhq/client";
 
 import { NotionProjectsDatabaseResponse } from "./types";
-import { env } from "@/app/lib/env/env";
+import { env } from "@/app/lib/env";
 
 const notion = new Client({ auth: env.NOTION_API_KEY });
 
